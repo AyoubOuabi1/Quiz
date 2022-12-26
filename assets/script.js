150e1a94687a40a0e65f61bcebd747510bf1e7c8
Ayoub Ouabi <ayoub.ouabi06@gmail.com>
2022-12-26 15:01:41 +0100

    add random function

--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,104 @@
 const progressDone = document.querySelector('.progress-done');
 let ClickedButton = document.querySelectorAll('button');
+let arrayNumber=[];
 function changeProgress(maxValue,finalValue){
     progressDone.style.width=`${(maxValue*100)/finalValue}%`;
     progressDone.innerHTML=`${Number((maxValue*100)/finalValue).toFixed(2)}%`;
 }
  getData();
+
+function printQuestion(data, arrayNumberCounter) {
+    $('#questionTitle').text(`${data[arrayNumber[arrayNumberCounter]].question}`)
+    $('#1').text(`${data[arrayNumber[arrayNumberCounter]].choice1}`);
+    $('#2').text(`${data[arrayNumber[arrayNumberCounter]].choice2}`);
+    $('#3').text(`${data[arrayNumber[arrayNumberCounter]].choice3}`);
+    $('#4').text(`${data[arrayNumber[arrayNumberCounter]].choice4}`);
+}
+
 function getData(){
+    $("#scrPar").css("display","none");
     $.ajax({
         "url": "./assets/questions.json",
         "dataType": "json",
         success: function(data){
-
-            $('#questionTitle').text(`${data[0].question}`)
-            $('#1').text(`${data[0].choice1}`);
-            $('#2').text(`${data[0].choice2}`);
-            $('#3').text(`${data[0].choice3}`);
-            $('#4').text(`${data[0].choice4}`);
-            changeProgress(0,data.length);
             let dataSize = data.length;
             let counter = 1;
             let index = 0;
+            let CorrectAnswer=0;
+            rangeNumber(dataSize);
+            console.log(arrayNumber);
+            printQuestion(data, 0);
+            changeProgress(0,data.length);
 
-            for(let i=0; i<4; i++) {
-                ClickedButton[i].addEventListener('click',function () {
-                    progressDone.style.marginLeft =`0px`;
-                    if(counter<dataSize){
-                        let ans=data[index].answer-1;
-                        changeCorrectAnswerBackground(ans,i)
-                        setTimeout(function () {
-                            for(let j=0;j<4;j++) {
-                                ClickedButton[j].classList.remove("answerNotCorrect");
-                                ClickedButton[j].classList.remove("answerCorrect");
-                                ClickedButton[j].classList.add("answerBefor");
+                for(let i=0; i<4; i++) {
+                    ClickedButton[i].addEventListener('click',function (qualifiedName, value) {
+                        progressDone.style.marginLeft =`0px`;
+                        if(counter<dataSize){
+                            //arrayNumberCounter++;
+                            if(data[[arrayNumber[index]]].answer==ClickedButton[i].getAttribute("id")){
+                                console.log("this is correct")
+                                CorrectAnswer++;
+                                ClickedButton[i].classList.add("answerCorrect");
+
+                            }else {
+                                ClickedButton[i].classList.add("answerNotCorrect");
+
                             }
-                            $('#questionTitle').text(`${data[counter].question}`)
-                            $('#1').text(`${data[counter].choice1}`);
-                            $('#2').text(`${data[counter].choice2}`);
-                            $('#3').text(`${data[counter].choice3}`);
-                            $('#4').text(`${data[counter].choice4}`);
+
+
+                            ClickedButton[i].setAttribute("disabled", value);
+                            setTimeout(function () {
+                                ClickedButton[i].removeAttribute("disabled");
+                                ClickedButton[i].classList.remove("answerNotCorrect");
+                                ClickedButton[i].classList.remove("answerCorrect");
+                                ClickedButton[i].classList.add("answerBefor");
+                                printQuestion(data, counter);
+                                changeProgress(counter,dataSize);
+                                counter++;
+                                index++;
+
+                            },500)
+
+                        }else if(counter===dataSize){
                             changeProgress(counter,dataSize);
-                            counter++;
-                            index++;
-                        },500)
+                            if(data[[arrayNumber[index]]].answer==ClickedButton[i].getAttribute("id")){
+                                console.log("this is correct")
+                                CorrectAnswer++;
+                                ClickedButton[i].classList.add("answerCorrect");
 
-                    }else if(counter===dataSize){
-                        changeProgress(counter,dataSize);
-                        let ans=data[index].answer-1;
-                        console.log(ans);
-                        changeCorrectAnswerBackground(ans,i)
-                        alert("thank you its last one")
-                    }
+                            }else {
+                                ClickedButton[i].classList.add("answerNotCorrect");
 
-                })
-            }
+                            }
+                            setTimeout(function (){
+                                $("#scr").html(`${CorrectAnswer}`);
+                                $("#scrPar").removeAttr("style");
+                                $("#answerPar").css("display","none");
+                            },500)
+
+
+
+                        }
+
+                    })
+                }
+
+
+
 
 
         }
     })
 }
-function changeCorrectAnswerBackground(answer,i){
 
-    ClickedButton[answer].classList.remove("answerBefor");
-    ClickedButton[answer].classList.remove("answerNotCorrect");
-    ClickedButton[answer].classList.add("answerCorrect");
-    for(let j=0;j<4;j++) {
-        if(j!==answer){
-            ClickedButton[i].classList.remove("answerBefor");
-            ClickedButton[j].classList.remove("answerCorrect");
-            ClickedButton[j].classList.add("answerNotCorrect");
+function rangeNumber(maxValue){
+    let number;
+    for (let i=0; i<maxValue;i++){
+        number = Math.floor(Math.random() * maxValue);
+        while (arrayNumber.includes(number)){
+            number = Math.floor(Math.random() * maxValue);
         }
+        arrayNumber.push(number);
     }
+
 }
